6d7b37782467fdfeb4c03e9a1fb11a238e8f6a9e
ngyn <72511692+ng-yn@users.noreply.github.com>
2021-04-23 02:16:28 +0100

    Added auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,14 @@
-import time
-from unittest import TestCase
-
-from flask_login import logout_user, LoginManager
-from flask_login import login_user
-
-from my_app import create_app, db
-from my_app.config import TestingConfig
 import pytest
+
+
+def signup(client, username, email, password, password_repeat):
+    """ Function to sign up an unregistered user"""
+    return client.post('/signup/', data=dict(
+            username=username,
+            email=email,
+            password=password,
+            password_repeat=password_repeat
+        ), follow_redirects=True)
 
 
 def login(client, email, password):
@@ -33,7 +35,7 @@ class TestAuth:
         response = client.get('/')
         assert response.status_code == 200
 
-    def test_pages_require_login(self, client):
+    def test_login_required_pages(self, client):
         """
         GIVEN A user is not logged in
         WHEN When they access the profile menu option
@@ -61,12 +63,7 @@ class TestAuth:
         """
         from my_app.models import User
         count = User.query.count()
-        response = client.post('/signup/', data=dict(
-            username='user2',
-            email='person_2@people.com',
-            password='password2',
-            password_repeat='password2'
-        ), follow_redirects=True)
+        response = signup(client, 'user2', 'user2@gmail.com', 'password2', 'password2')
         count2 = User.query.count()
         assert response.status_code == 200
         assert b'user2' in response.data
@@ -74,7 +71,27 @@ class TestAuth:
         assert b'Hello, user2. You have successfully signed up. Please login to continue.'
 
     @pytest.mark.usefixtures('user')
-    def test_login(self, client):
+    def test_signup_fails(self, client):
+        """
+        GIVEN A user is not registered
+        WHEN When they submit an invalid registration form
+        (i.e. email already exists, incorrect password length and password not matching)
+        THEN they should be flashed with invalid credential errors
+        """
+        response = signup(client, 'user2', 'testuser@gmail.com', 'password2', 'password2')
+        assert response.status_code == 200
+        assert b'An account is already registered for that email address' in response.data
+
+        response = signup(client, 'user2', 'user2@gmail.com', 'pas', 'pas')
+        assert response.status_code == 200
+        assert b'Password must be between 4-16 characters long' in response.data
+
+        response = signup(client, 'user2', 'user2@gmail.com', 'password2', 'password')
+        assert response.status_code == 200
+        assert b'Passwords must match' in response.data
+
+    @pytest.mark.usefixtures('user')
+    def test_login_succeeds(self, client):
         """
         GIVEN A user is registered
         WHEN When they login
@@ -85,10 +102,24 @@ class TestAuth:
         assert b'Hello, testuser. You have successfully logged in.' in response.data
 
     @pytest.mark.usefixtures('user')
+    def test_login_fails(self, client):
+        """
+        GIVEN A user is registered
+        WHEN they login with incorrect details (i.e. wrong email or password)
+        THEN they should be flashed with an incorrect login details error
+        """
+        response = login(client, 'testuser@gmail.comx', 'password1')
+        assert response.status_code == 200
+        assert b'This email address is not registered'
+        response = login(client, 'testuser@gmail.com', 'password1x')
+        assert response.status_code == 200
+        assert b'The password is incorrect'
+
+    @pytest.mark.usefixtures('user')
     def test_logout(self, client):
         """
         GIVEN A user is logged in
-        WHEN When they logout
+        WHEN they logout
         THEN they the should be redirected to the home page and flashed with a logout message
         """
         response = login(client, 'testuser@gmail.com', 'password1')
@@ -97,4 +128,9 @@ class TestAuth:
         response = logout(client)
         assert response.status_code == 200
         assert b'You have successfully logged out.' in response.data
+
+
+
+
+
 
