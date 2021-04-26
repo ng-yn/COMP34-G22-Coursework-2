44771fc4a57ee9c58be53a88cf02ea8a7aea1cdc
ben <zcemhem@ucl.ac.uk>
2021-04-26 20:08:12 +0800

    branch update

--- my_app/snp500/snp500.py
+++ my_app/snp500/snp500.py
@@ -1,6 +1,5 @@
 # S&P 500 index page
-from io import BytesIO, StringIO
-import base64
+# created by Yu-Hsiang Chen
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify, flash, send_file
 from my_app.snp500.forms import SearchBar
 from my_app.models import Fundamentals
@@ -8,10 +7,10 @@ from my_app import db
 import sqlite3
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.offline as py
+
 
 snp500_bp = Blueprint('snp500_bp', __name__, url_prefix='/index')
-# conn = sqlite3.connect('../data/FinancialDatabase.db')
+
 
 
 @snp500_bp.route('/', methods=['GET', 'POST'])
@@ -75,53 +74,8 @@ def plotly_graph_object():
         legend_title="Technical Data",
         font=dict(size=18, color="Black")
     )
-    # return fig
+
     return fig.write_html('static/img/' + "{}".format(term) + '.html',
                           full_html=False,
                           include_plotlyjs='cdn')
-    # img = StringIO()
-    # fig.savefig(img, format='png')
-    # fig.close()
-    # img.seek(0)
-    # plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-    # return render_template('search_result.html', plot_url=plot_url)
-    # fig.show()
-    # fig.write_html("my_app/templates/stock_graph.html")
-    # html_div = py.plot(fig, output_type='div')
-    # return fig.write_image("templates/stock.jpg")
-    # return send_file(img, mimetype='image/png')
 
-    # text = request.args.get('ticker')
-    # query = db.session.query(Fundamentals).filter(Fundamentals.Symbol)
-    # results = db.session.query(search).filter(Fundamentals.(search)).all()
-    #
-    # ticker = request.form.get("autocomplete_tickers")
-    # all_tickers = [item.Symbol for item in Fundamentals.query.all()]
-    # if ticker not in all_tickers:
-    #     flash("Invalid ticker")
-    #     return redirect(url_for('snp500.index'))
-    # if ticker in all_tickers:
-    #     flash("Successful")
-    # # try:
-    # #     watchlist_item = WatchlistItem(user_id=current_user.get_id(), watchlist_id=watchlist_id, ticker=ticker)
-    # #     db.session.add(watchlist_item)
-    # #     db.session.commit()
-    # # except Exception as e:
-    # #     flash("Failed to add ticker")
-    # return redirect(url_for('snp500.index'))
-# def search():
-#     if request.method == 'POST':
-#         term = request.form['search_term']
-#         if term == "":
-#             flash("Enter a name to search for")
-#             return redirect(url_for('index'))
-#     #users = with_polymorphic(User, [Company_name])
-#     #results = db.session.query(Company_name).filter(
-#         #Company_name.Symbol.contains(term)).all()
-#         results = Company_name.query.filter(Company_name.Symbol.contains(term)).all()
-#         if not results:
-#             flash("No company found with that label.")
-#             return redirect(url_for('index'))
-#         return render_template('search_bar.html', results=results)
-#     else:
-#         return redirect(url_for('index'))
